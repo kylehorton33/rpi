8441696b9826b882786ce75d3a877aea94b957bb
kylehorton33 <kylehorton33@gmail.com>
2020-10-23 14:44:58 -0400

    no need for regex

--- lcd/lcd_demo.py
+++ lcd/lcd_demo.py
@@ -1,23 +1,23 @@
 # https://rplcd.readthedocs.io/en/latest/getting_started.html
 from RPLCD.i2c import CharLCD
-import re
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=16, rows=2, dotsize=8,
               charmap='A02',
-              auto_linebreaks=True,
+              auto_linebreaks=False,
               backlight_enabled=True)
-              
 
 lines = [
   "abcdefghijklmnop123",
-  "It works!"
+  "It works! but you can't see the rest of the text"
 ]
 
 for i,line in enumerate(lines):
-  char_count = len(re.findall('[a-zA-Z]',line))
-  print(char_count)
+  char_count = len(line)
+  #print(char_count)
   if char_count > 16:
-    print(f"The message \'{line}\' is too long to display!")
-  lcd.cursor_pos(i,0)
+    print(f"The message on LINE {i} is too long to display!")
+    print(f"Only displaying:\t\'{line[0:16]}\'")
+    print(f"Should see:\t\t\'{line}\'")
+  lcd.cursor_pos = (i,0)
   lcd.write_string(line)
