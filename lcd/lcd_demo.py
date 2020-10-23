6ccc22d6260d77e3ef6d4285ade8ae66e93946fa
Kyle <kylehorton33@gmail.com>
2020-10-23 14:12:20 -0400

    update to readthedocs demo

--- lcd/lcd_demo.py
+++ lcd/lcd_demo.py
@@ -1,21 +1,10 @@
-#Include libraries
-import RPi.GPIO as GPIO 
-import time
-from RPLCD.gpio import CharLCD
+# https://rplcd.readthedocs.io/en/latest/getting_started.html
+from RPLCD.i2c import CharLCD
 
-# Configure the LCD
-lcd = CharLCD(pin_rs = 19, pin_rw = None, pin_e = 16, pins_data = [21,18,23,24], 
-numbering_mode = GPIO.BOARD)
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=16, rows=2, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True,
+              backlight_enabled=True)
 
-# Create a variable 'number'
-number = 0
-
-# Main loop
-while(True):
-	# Increment the number and then print it to the LCD number = number + 1
-	lcd.clear()
-	lcd.write_string("Count: "+ str(number))
-	time.sleep(1) 
-
-lcd.close() 
-GPIO.cleanup()
+lcd.write_string('Hello world')
--- lcd/lcd_forum.py
+++
@@ -1,5 +0,0 @@
-import lcddriver
-display = lcddriver.lcd()
-display.lcd_clear()
-display.lcd_display_string("ABCDEFGHIJ", 1)
-display.lcd_display_string("123456789", 2)
