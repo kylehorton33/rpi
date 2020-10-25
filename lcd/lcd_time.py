347e09bc6713cf3fa4ded793ee1ad611d3fb6024
Kyle <kylehorton33@gmail.com>
2020-10-25 17:20:45 -0400

    script to display time

---
+++ lcd/lcd_time.py
@@ -0,0 +1,23 @@
+# https://rplcd.readthedocs.io/en/latest/getting_started.html
+from RPLCD.i2c import CharLCD
+import datetime
+
+## Pinout
+# GND - Pin 6
+# VCC - Pin 4
+# SDA - Pin 3
+# SCL - Pin 5
+
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=16, rows=2, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=False,
+              backlight_enabled=True)
+
+lcd.cursor_pos = (0,0)
+lcd.write_string("Current Time:")
+
+while(True):
+  lcd.cursor_pos = (1,0)
+  lcd.write_string(datetime.datetime.now())
+
