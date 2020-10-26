03f17497de9fe1cf515325bb7f6de719be9d830a
Kyle <kylehorton33@gmail.com>
2020-10-26 18:06:37 -0400

    rename id_

--- rfid/login.py
+++ rfid/login.py
@@ -11,21 +11,21 @@ with open('users/users.csv', mode='r') as infile:
     reader = csv.reader(infile)
     users = {rows[0]:rows[1] for rows in reader}
 
-id = 0
+id_ = 0
 while not id:
   print("scan badge to login")
   try:
-    id, text = reader.read()
+    id_, text = reader.read()
     now = datetime.datetime.now()
 
-    if id in users:
-      print(f"\n\tHello, {users[id]}")
-      print(f"\tScanned {users[id]} at {now}\n")
+    if id_ in users:
+      print(f"\n\tHello, {users[id_]}")
+      print(f"\tScanned {users[id_]} at {now}\n")
       time.sleep(2)
     elif id:
       print("\n\tUser not recognized")
       time.sleep(2)
-  id = 0
+  id_ = 0
   else:
     print("\tsomething went wrong...")
     time.sleep(2)
